c365d93c884ab07e423a94d3ba3152669d4cc08b
Thiago Luiz Silva <contato@thluiz.com>
2022-10-15 19:37:18 -0300

    First commit

---
+++ SIG/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SIG;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
