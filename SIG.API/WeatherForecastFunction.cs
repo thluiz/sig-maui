d716bd6affa7438d9d24620f7fc5674c54c9e94f
Thiago Luiz Silva <contato@thluiz.com>
2022-10-15 21:23:24 -0300

    Testing API function

---
+++ SIG.API/WeatherForecastFunction.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+using SIG.Shared;
+
+namespace SIG.API
+{
+    public class WeatherForecastFunction
+    {
+        public readonly WeatherForecastService Service;
+
+        public WeatherForecastFunction()
+        {
+            Service = new WeatherForecastService();
+        }
+
+        private static string GetSummary(int temp)
+        {
+            var summary = "Mild";
+
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing!";
+            }
+
+            return summary;
+        }
+
+        [FunctionName("WeatherForecast")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var result = await Service.GetForecastAsync(DateTime.Now);
+
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ SIG.Shared/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIG.Shared;
+
+public class WeatherForecastService
+{
+	private static readonly string[] Summaries = new[]
+	{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
+
+	public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+    {
+        var random = new Random();
+
+		return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = startDate.AddDays(index),
+            TemperatureC = random.Next(-20, 55),
+			Summary = Summaries[random.Next(Summaries.Length)]
+		}).ToArray());
+	}
+}
+
