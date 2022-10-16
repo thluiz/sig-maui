69afb828c442fef375f9bf9166f15532c32d3de7
Thiago Luiz Silva <contato@thluiz.com>
2022-10-15 23:34:23 -0300

    ignoring built-in login

--- SIG.Blazor/wwwroot/service-worker.js
+++ SIG.Blazor/wwwroot/service-worker.js
@@ -1,4 +1,10 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.addEventListener('fetch', () => {
+
+    if (event.request.url.indexOf('.auth/') !== -1) {
+        return false;
+    }
+
+});
