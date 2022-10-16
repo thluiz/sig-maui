4176008eaac265e1ac1e9053f6bc116956bc28c3
Thiago Luiz Silva <contato@thluiz.com>
2022-10-16 20:07:48 -0300

    Adding nuget packages to new function project

---
+++ Sig.FunctionApi/Startup.cs
@@ -0,0 +1,26 @@
+﻿using AzureFunctions.Extensions.OpenIDConnect.Configuration;
+using AzureFunctions.Extensions.OpenIDConnect.InProcess.Configuration;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Sig.FunctionApi;
+using ConfigurationBuilder = Microsoft.Extensions.Configuration.ConfigurationBuilder;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Sig.FunctionApi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.AddOpenIDConnect(config =>
+            //{
+            //    config.SetTokenValidation(TokenValidationParametersHelpers.Default("sig-function-api.myvtmi.im", "https://dev-regf5o5f.us.auth0.com/"));
+            //    config.SetIssuerBaseUrlConfiguration("https://dev-regf5o5f.us.auth0.com/");
+            //});
+        }
+    }
+}
---
+++ Sig.FunctionApi/WeatherForecastFunction.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using SIG.Shared;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SIG.API
+{
+    public class WeatherForecastFunction
+    {
+        public readonly WeatherForecastService Service;
+
+        public WeatherForecastFunction()
+        {
+            Service = new WeatherForecastService();
+        }
+
+
+        //[Authorize]
+        //[FunctionName("WeatherForecast")]
+        //public async Task<IActionResult> Run(
+        //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        //    ILogger log)
+        //{
+
+        //    var result = await Service.GetForecastAsync(DateTime.Now);
+            
+        //    var userId = req.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+        //    return new OkObjectResult(result.Select(r =>
+        //    {
+        //        r.Summary = userId;
+        //        return r;
+        //    }));
+        //}
+    }
+}
