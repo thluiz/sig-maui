3255a9964d6f8c288ba5e01a2d47c3b1fb96070b
Thiago Luiz Silva <contato@thluiz.com>
2022-10-23 16:39:24 -0300

    Updating icon and Splash Screen

--- SIG.Blazor/wwwroot/service-worker.published.js
+++ SIG.Blazor/wwwroot/service-worker.published.js
@@ -4,7 +4,10 @@
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+//Disable app cache while developing
+//self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+self.addEventListener('fetch', () => { });
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
